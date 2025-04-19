388ccf548a51eeb9cbc43698a44a7aaacfc6c2e4
Swaraj Mishra <cse.22bcsb72@silicon.ac.in>
2025-04-19 16:11:33 +0530

    Swaraj

--- webwiz/wiki-pedia/backup/src/components/Article/article.jsx
+++ webwiz/wiki-pedia/backup/src/components/Article/article.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
+// eslint-disable-next-line no-unused-vars
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 import {
